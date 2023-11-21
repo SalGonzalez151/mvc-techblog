744e1b832b3b7aca9495ba7287e34f1bf6fb5dd9
Sal Gonzalez <imdone151@gmail.com>
2023-11-21 14:31:06 -0800

    added dashboardRoute and userRoute code

---
+++ controllers/api/dashboardRoute.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Dashboard } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const dashboardData = await Dashboard.create({
+            ...req.body,
+            user_id: req.session.user_id,    
+        })
+        res.status(200).json(dashboardData) 
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const dashboardData = await Dashboard.update({
+            ...req.body,
+            date_updated: new Date()
+        }, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        })
+        if (!dashboardData) {
+            res.status(404).json("Post not found")
+        }
+        res.status(200).json(dashboardData)
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const dashboardData = await Dashboard.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,    
+            }
+        })
+        res.status(200).json(dashboardData)
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+})
+
+module.exports = router;
---
+++ controllers/api/userRoute.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body)
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+
+            res.status(200).json(userData);
+        })
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
+
+//log in the user
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {email: req.body.email}})
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password'})
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({message: 'incorrect email or password'})
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id,
+            req.session.loggedIn = true;
+
+            res.status(200).json({ user: userData, message: 'You are logged in'})
+        })
+    } catch (err) {
+        res.status(400).json(err.message)
+    }
+})
+
+router.post('/logout', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            req.session.destroy(() => {
+                res.status(204).end()
+            })
+        } else {
+            res.status(404).end()
+        }
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+     
+})
+
+module.exports = router;

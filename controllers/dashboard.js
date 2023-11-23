cd45c3936b536d10cf8b8c835f003deb288300da
Sal Gonzalez <imdone151@gmail.com>
2023-11-22 16:35:29 -0800

    added dashboard.js update and delete code and add public js code

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -12,8 +12,46 @@ router.post('/', async (req, res) => {
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(newProject);
+      res.status(200).json(dashboardData);
     } catch (err) {
       res.status(400).json(err);
     }
   });
+
+  router.get('/dashboard/:id', async (req, res) => {
+    try {
+        const dashboardData = await Dashboard.findByPk(req.params.id, {
+            include: { model: User }
+        })
+        const dashboard = dashboardData.get({ plain: true })
+        res.render("singleProject", {
+            ...dashboard,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json(err.message)
+    }
+})
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const dashboardData = await Dashboard.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!projectData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(projectData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
